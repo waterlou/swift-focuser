837a2d313f7c020012c074dc36a0bfcaf9545b13
Water Lou <water@waterworld.com.hk>
2023-10-11 16:58:48 +0800

    bump introspect to 1.1.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["Focuser"]),
     ],
     dependencies: [
-        .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.3")
+        .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "1.1.0")
     ],
     targets: [
         .target(
